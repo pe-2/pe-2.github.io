4e33fc87640c44f0c1f58575d72a4259460a8500
pe-2 <2469831325@qq.com>
2021-05-16 21:13:29 +0800

    vedio

--- dog-mobile/dogvedio/js/scroll.js
+++ dog-mobile/dogvedio/js/scroll.js
@@ -2,9 +2,9 @@ var button1 = document.getElementsByClassName('intro-nav')[0];
 var button2 = document.getElementsByClassName('comments')[0];
 var recommendBox = document.getElementsByClassName('vedio-recom')[0];
 var commitBox = document.getElementsByClassName('vedio-comments')[0];
-
+var width = commitBox.offsetWidth;
 var scrollBox = document.getElementsByClassName('vedio-else-wrapper')[0];
-
+var vedio = document.getElementsByTagName('video')[0];
 button1.onclick = function() {
     button1.className += " yellow";
     button1.style.cssText = "border-bottom: 0.1px solid #ffc300;margin-top:0.5em;padding-bottom:0.5em;";
@@ -17,18 +17,18 @@ button2.onclick = function() {
     button2.style.cssText = "border-bottom: 0.1px solid #ffc300;margin-top:0.5em;padding-bottom:0.5em;";
     button1.style.cssText = "";
     button1.className = "intro-nav";
-    scrollBox.scrollLeft = 360;
+    scrollBox.scrollLeft = width;
 }
 scrollBox.ontouchend = function() {
-    if (scrollBox.scrollLeft >= 0 && scrollBox.scrollLeft <= 180) {
+    if (scrollBox.scrollLeft >= 0 && scrollBox.scrollLeft <= width / 2) {
         scrollBox.scrollLeft = 0;
         button1.className += " yellow";
         button1.style.cssText = "border-bottom: 0.1px solid #ffc300;margin-top:0.5em;padding-bottom:0.5em;";
         button2.style.cssText = "";
         button2.className = "comments"
 
-    } else if (scrollBox.scrollLeft > 180 && scrollBox.scrollLeft < 360) {
-        scrollBox.scrollLeft = 360;
+    } else if (scrollBox.scrollLeft > width / 2 && scrollBox.scrollLeft < width) {
+        scrollBox.scrollLeft = width;
         button2.className += " yellow";
         button2.style.cssText = "border-bottom: 0.1px solid #ffc300;margin-top:0.5em;padding-bottom:0.5em;";
         button1.style.cssText = "";
@@ -43,4 +43,16 @@ function runleft() {
 
 function runright() {
     setInterval(function() {}, );
+}
+
+
+var btn = document.getElementsByClassName('playoronpause')[0];
+var count = 0;
+btn.onclick = function() {
+    if (count % 2 == 0) {
+        vedio.play();
+    } else {
+        vedio.pause();
+    }
+    count++;
 }
